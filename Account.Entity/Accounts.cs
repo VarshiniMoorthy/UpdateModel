d474b38bf732d5aedc40849d0eb39f3f9373d177
VarshiniMoorthy <50829652+VarshiniMoorthy@users.noreply.github.com>
2020-02-20 22:48:09 +0530

    Add files via upload

---
+++ Account.Entity/Accounts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account.Entity
+{
+    public class Accounts
+    {
+        public string EmailId { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public byte Age { get; set; }
+        public string City { get; set; }
+        public string PhoneNumber { get; set; }
+        public string LanguageKnown { get; set; }
+
+        public Accounts(string id, string password, string gender, byte age,string city,string phoneNo,string language)
+        {
+            this.EmailId = id;
+            this.Password = password;
+            this.Gender = gender;
+            this.Age = age;
+            this.City = city;
+            this.PhoneNumber = phoneNo;
+            this.LanguageKnown = language;
+
+        }
+        public Accounts()
+        {
+
+        }
+
+    }
+}
